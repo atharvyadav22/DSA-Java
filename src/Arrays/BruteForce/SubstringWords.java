1ae97df4c24e3da8256ce26ee6b3054ca140bef6
atharvyadav22 <dev.atharvyadav@gmail.com>
2024-12-26 17:12:02 +0530

    BruteForce Method

---
+++ src/Arrays/BruteForce/SubstringWords.java
@@ -0,0 +1,18 @@
+package Arrays.BruteForce;
+
+//Problem No-1967 https://leetcode.com/problems/number-of-strings-that-appear-as-substrings-in-word/
+public class SubstringWords {
+
+    public int numOfStrings(String[] patterns, String word) {
+
+        int count = 0;
+
+        for (String p : patterns) {
+            if (word.contains(p)) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+}
