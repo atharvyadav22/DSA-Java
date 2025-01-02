32e6caeb442e98f75a3134729d233f9282248c2a
atharvyadav22 <dev.atharvyadav@gmail.com>
2025-01-02 23:52:19 +0530

    BruteForce/Pascals Triangle

---
+++ src/Arrays/BruteForce/PascalsTriangle.java
@@ -0,0 +1,31 @@
+package Arrays.BruteForce;
+
+import java.util.ArrayList;
+import java.util.List;
+
+//Problem No-118 https://leetcode.com/problems/pascals-triangle/?envType=problem-list-v2&envId=array
+public class PascalsTriangle {
+    public static List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> result = new ArrayList<>();
+
+        for (int i = 0; i < numRows; i++) {
+            result.add(nCr(i));
+        }
+
+        return result;
+    }
+
+    public static List<Integer> nCr(int r) {
+        List<Integer> row = new ArrayList<>();
+        long coefficient = 1;
+
+        row.add(1);
+
+        for (int k = 1; k <= r; k++) {
+            coefficient = coefficient * (r - k + 1) / k;
+            row.add((int) coefficient);
+        }
+
+        return row;
+    }
+}
