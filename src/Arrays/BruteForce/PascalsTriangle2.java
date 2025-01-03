67413862ebd13dc833a03f801899e912fc2f5b81
atharvyadav22 <dev.atharvyadav@gmail.com>
2025-01-03 22:24:25 +0530

    BruteForce/Pascals Triangle 2

---
+++ src/Arrays/BruteForce/PascalsTriangle2.java
@@ -0,0 +1,22 @@
+package Arrays.BruteForce;
+
+import java.util.ArrayList;
+import java.util.List;
+
+//Problem No-119 https://leetcode.com/problems/pascals-triangle-ii/?envType=problem-list-v2&envId=dynamic-programming
+public class PascalsTriangle2 {
+    public List<Integer> getRow(int rowIndex) {
+
+        List<Integer> row = new ArrayList();
+        long coeff = 1;
+
+        row.add(1);
+
+        for(int i=1; i<=rowIndex; i++){
+            coeff = coeff *(rowIndex - i +1)/i;
+            row.add( (int) coeff);
+        }
+
+        return row;
+    }
+}
