bdc0d8c698671971c03657e93dfccd0200983e8a
atharvyadav22 <dev.atharvyadav@gmail.com>
2025-01-17 23:18:56 +0530

    Arrays/BureForce/Plus One

---
+++ src/Arrays/BruteForce/PlusOne.java
@@ -0,0 +1,23 @@
+package Arrays.BruteForce;
+
+//Problem No-66 https://leetcode.com/problems/plus-one/description/?envType=problem-list-v2&envId=array
+public class PlusOne {
+    public int[] plusOne(int[] digits) {
+
+        for(int i = digits.length-1; i >=0; i--){
+            digits[i] += 1;
+
+            if(digits[i] == 10){
+                digits[i] = 0;
+
+            }
+            else {
+                return digits;
+            }
+        }
+
+        int[] result = new int[digits.length+1];
+        result[0] = 1;
+        return result;
+    }
+}
