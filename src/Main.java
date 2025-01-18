c4c0271a28a446e431504c6ba5514266a16ea649
atharvyadav22 <dev.atharvyadav@gmail.com>
2025-01-18 22:00:32 +0530

    Hashing/Maximum No Balloons

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
 
+        System.out.println(sum(5));
+    }
+
+    static int sum (int n){
+
+        if(n ==1) return 1;
+        int ans = sum(n-1);
+        return ans;
     }
 }
