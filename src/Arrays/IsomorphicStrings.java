48c3ffd04f22212b7bf93ab03cf01d512e98802d
atharvyadav22 <dev.atharvyadav@gmail.com>
2025-01-21 23:19:32 +0530

    Arrays/Isomorphic Strings

---
+++ src/Arrays/IsomorphicStrings.java
@@ -0,0 +1,28 @@
+package Arrays;
+
+//Problem No-205 https://leetcode.com/problems/isomorphic-strings/description/
+public class IsomorphicStrings {
+    public boolean isIsomorphic(String s, String t) {
+
+        if (s.length() != t.length()) {
+            return false;
+        }
+
+        int[] map1 = new int[256];
+        int[] map2 = new int[256];
+
+        for (int i = 0; i < s.length(); i++) {
+            char sChar = s.charAt(i);
+            char tChar = t.charAt(i);
+
+            if (map1[sChar] == 0 && map2[tChar] == 0) {
+                map1[sChar] = tChar;
+                map2[tChar] = sChar;
+            } else if (map1[sChar] != tChar || map2[tChar] != sChar) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
