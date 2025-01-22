d5becea925148769393379511d20f0fa502acc25
atharvyadav22 <dev.atharvyadav@gmail.com>
2025-01-22 23:50:38 +0530

    Arrays/Ransom Note

---
+++ src/Arrays/RansomNote.java
@@ -0,0 +1,24 @@
+package Arrays;
+
+//Problem No-383 https://leetcode.com/problems/ransom-note/description/
+public class RansomNote {
+    public boolean canConstruct(String ransomNote, String magazine) {
+
+        int[] arr = new int[26];
+
+        for(char c: magazine.toCharArray()){
+            arr[c - 'a']++;
+        }
+
+        for(char c: ransomNote.toCharArray()){
+
+            if(arr[c - 'a'] == 0){
+                return false;
+            }
+
+            arr[c - 'a']--;
+        }
+
+        return true;
+    }
+}
