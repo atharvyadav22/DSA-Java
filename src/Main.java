f2bb9ab22fc6b34a4983b105fa01290d58744d1e
atharvyadav22 <dev.atharvyadav@gmail.com>
2025-02-07 23:09:33 +0530

    Strings/PrefixSum/Maximum Score After Splitting String

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,7 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println(sum(5));
-    }
-
-    static int sum (int n){
-
-        if(n ==1) return 1;
-        int ans = sum(n-1);
-        return ans;
     }
 }
