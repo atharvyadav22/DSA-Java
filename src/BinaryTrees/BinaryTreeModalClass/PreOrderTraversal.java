a95e946d4b7532dad5619ed33c741162eec41ef1
atharvyadav22 <dev.atharvyadav@gmail.com>
2025-09-05 21:07:58 +0530

    BinaryTree/PreOrderTraversal

--- src/BinaryTrees/BinaryTreeModalClass/PreOrderTraversal.java
+++ src/BinaryTrees/BinaryTreeModalClass/PreOrderTraversal.java
@@ -1,26 +1,24 @@
-package BinaryTrees;
+package BinaryTrees.BinaryTreeModalClass;
 
-import BinaryTrees.BinaryTreeModalClass.TreeNode;
 import java.util.ArrayList;
 import java.util.List;
 
-//Problem No- 94 https://leetcode.com/problems/binary-tree-inorder-traversal/description/?envType=problem-list-v2&envId=binary-tree
-public class PostOrderTraversal {
+//Problem No- 144 https://leetcode.com/problems/binary-tree-preorder-traversal/description/?envType=problem-list-v2&envId=binary-tree
+public class PreOrderTraversal {
 
-    public List<Integer> inorderTraversal(TreeNode root) {
+    public List<Integer> preorderTraversal(TreeNode root) {
         List<Integer> list = new ArrayList<>();
         helper(root, list);
         return list;
     }
-
 
     public void helper(TreeNode root, List<Integer> list) {
         if (root == null) {
             return;
         }
 
-        helper(root.left, list);
         list.add(root.val);
+        helper(root.left, list);
         helper(root.right, list);
     }
 }
